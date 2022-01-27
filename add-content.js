3fd559c11cc2d86263f8f7fdf5d978d7d5299415
Christopher Yamas <yamas.chris@gmail.com>
2022-01-27 17:02:39 -0600

    html, css, java, images created

---
+++ add-content.js
@@ -0,0 +1,23 @@
+// SETTING VARIABLE THROUGH PROMPT
+let pawetry = prompt("Yes or No: are you ready for some PAW-etry?")
+
+// IF TRUE
+if (pawetry == 'Yes') {
+    alert('aw HECK YEAH!')
+  }
+  // ELSE a.k.a OTHERWISE ->  FALSE
+  else {
+    alert('WELL YOU BETTER GET READY')
+}
+
+// SETTING ANOTHER VARIABLE THROUGH PROMPT
+let catLove = prompt("Oh and i meant to ask: Yes or No...are you cool with some fun cat pics?")
+
+// IF TRUE
+if (catLove == 'Yes') {
+    alert('wow you really came to the right place')
+  }
+  // ELSE a.k.a OTHERWISE ->  FALSE
+  else {
+    alert('lol well regardless, here they are...')
+}
