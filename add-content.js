2b813cf18fa793529fc6e02ba5b62855778419f7
Christopher Yamas <yamas.chris@gmail.com>
2022-01-27 20:44:28 -0600

    debugged error message

--- add-content.js
+++ add-content.js
@@ -1,11 +1,11 @@
 function promptCatScale() {
     // step 1: prompt user for Cat Love number
-    let cScale = prompt('On a scale of 1-10, how much do you love cats?').toLowerCase.trim();
+    let cScale = prompt('On a scale of 1-10, how much do you love cats?');
     console.log('cScale', typeof Number(cScale));
     // Step 2: decide what response to return to the user
     let response = 'so you like to play games, eh?! let\'s try that again...';
-    alert(response)
-    promptCatScale()
+    alert(response);
+    promptCatScale();
     // Step 3: create logic that changes reponse
     // Step 4: check user input for valid input (its a number)
     // IF not valid, call promptCatScale() again.
