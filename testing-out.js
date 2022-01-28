2f786e06ae26f0115323de3bde1f1984c69eff78
Christopher Yamas <yamas.chris@gmail.com>
2022-01-27 20:27:51 -0600

    improved js to provide error message and require prompt

--- testing-out.js
+++ testing-out.js
@@ -1,11 +1,17 @@
 function promptCatScale() {
-    // step 1: prompt user for Cat Love number
+    let confirm = window.confirm('Would you like to see some of Christopher\'s Cat Poetry?');
     let cScale = prompt('On a scale of 1-10, how much do you love cats?').toLowerCase.trim();
+
+    
+}
+
+___________________
+function promptCatScale() {
+    // step 1: prompt user for Cat Love number
+    let cScale = prompt('On a scale of 1-10, how much do you love cats?')
     console.log('cScale', typeof Number(cScale));
     // Step 2: decide what response to return to the user
-    let response = 'so you like to play games, eh?! let\'s try that again...';
-    alert(response)
-    promptCatScale()
+    let response = 'so you like to play games, eh?!';
     // Step 3: create logic that changes reponse
     // Step 4: check user input for valid input (its a number)
     // IF not valid, call promptCatScale() again.
