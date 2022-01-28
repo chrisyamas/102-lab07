bf573e92eff78d85bf63c10d0fd26a543f317cad
Christopher Yamas <yamas.chris@gmail.com>
2022-01-27 19:24:25 -0600

    fixed link to js in html, made all new js prompts

--- add-content.js
+++ add-content.js
@@ -1,23 +1,21 @@
-// SETTING VARIABLE THROUGH PROMPT
-let pawetry = prompt("Yes or No: are you ready for some PAW-etry?")
-
-// IF TRUE
-if (pawetry == 'Yes') {
-    alert('aw HECK YEAH!')
+function promptCatScale() {
+    // step 1: prompt user for Cat Love number
+    let cScale = prompt('On a scale of 1-10, how much do you love cats?')
+    console.log('cScale', typeof Number(cScale));
+    // Step 2: decide what response to return to the user
+    let response = 'so you like to play games, eh?!';
+    // Step 3: create logic that changes reponse
+    // Step 4: check user input for valid input (its a number)
+    // IF not valid, call promptCatScale() again.
+    if (cScale > 7) {
+      response = 'Oh heck yeah! You\'re in the right place, my friend!';
+    } else if (cScale > 5) {
+      response = 'Alright, I don\'t love it but I respect you!';
+    } else if (cScale >= 0) {
+      response = 'Um...who hurt you? You probably have an anxious attachment style.';
+    }
+  
+    return alert(response)
   }
-  // ELSE a.k.a OTHERWISE ->  FALSE
-  else {
-    alert('WELL YOU BETTER GET READY')
-}
 
-// SETTING ANOTHER VARIABLE THROUGH PROMPT
-let catLove = prompt("Oh and i meant to ask: Yes or No...are you cool with some fun cat pics?")
-
-// IF TRUE
-if (catLove == 'Yes') {
-    alert('wow you really came to the right place')
-  }
-  // ELSE a.k.a OTHERWISE ->  FALSE
-  else {
-    alert('lol well regardless, here they are...')
-}
+  promptCatScale()
